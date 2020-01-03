6722d73bd4c0028f8c36dc0fe468a0b077cd33c7
katunold <katunold94@gmail.com>
2020-01-03 19:18:53 +0300

    - implement json patch functionality

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import dotenv from 'dotenv';
 import app from './utils/express';
 import router from './routes/index';
 
 const PORT = 8000;
-dotenv.config();
 
 app.get('/', (req, res) => {
   res
@@ -11,6 +9,14 @@ app.get('/', (req, res) => {
     .send({ message: "Hackerbay.io backend challenge" });
 });
 app.use(router);
+
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).send({ UnauthorizedError: err.message });
+  }
+  next();
+});
+
 
 app.server = app.listen(PORT, () => {
   console.log(`server has started on port ${PORT}`)
