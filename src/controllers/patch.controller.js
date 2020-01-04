becdfe6a1ff4e28e5eb7df37fa78dd65368ab0c2
katunold <katunold94@gmail.com>
2020-01-04 23:15:27 +0300

    - document app using swagger

--- src/controllers/patch.controller.js
+++ src/controllers/patch.controller.js
@@ -2,11 +2,11 @@ import * as jsonPatch from 'jsonpatch';
 import user from '../models/user.model';
 
 export default class PatchController {
+  static thePatch = [];
   static patchUserData = (req, res) => {
     const { body } = req;
     const userKeys = Object.keys(user);
     const reqKeys = Object.keys(body);
-    const thePatch = [];
 
     reqKeys.forEach((key) => {
       let dataObj;
@@ -16,12 +16,12 @@ export default class PatchController {
           path: `/${key}`,
           value: `${body[key]}`,
         };
-        thePatch.push(dataObj);
+        this.thePatch.push(dataObj);
       }
       dataObj = { op: 'add', path: `/${key}`, value: `${body[key]}` };
-      thePatch.push(dataObj);
+      this.thePatch.push(dataObj);
     });
-    const patchedDoc = jsonPatch.apply_patch(user, thePatch);
+    const patchedDoc = jsonPatch.apply_patch(user, this.thePatch);
 
     return res.status(200).send(patchedDoc);
   };
--- src/utils/express.js
+++ src/utils/express.js
@@ -4,6 +4,8 @@ import compress from 'compression';
 import helmet from 'helmet';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
+import swagger from 'swagger-ui-express';
+import swaggerDoc from '../swagger/swagger';
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,5 +14,5 @@ app.use(cors());
 app.use(helmet());
 app.use(compress());
 app.use(logger('dev'));
-
+app.use('/docs', swagger.serve, swagger.setup(swaggerDoc, { explorer: true }));
 export default app;
