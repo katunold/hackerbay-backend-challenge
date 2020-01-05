993d9c2fdf980319c84b5cc52b727bf3180c0483
katunold <katunold94@gmail.com>
2020-01-05 16:42:19 +0300

    - update readme file

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,15 +5,19 @@ import errorDisplay from '../middleware';
 
 export default class AuthController {
   static userLogin = (req, res) => {
-    const { body } = req;
-    const { userId, userName, password } = user;
+    const { userName, password } = req.body;
+    const { userId } = user;
+    const userDataValues = Object.values(user);
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       return errorDisplay(req, res, errors);
     }
 
-    if (body.userName === userName && body.password === password) {
+    if (
+      userDataValues.includes(userName) &&
+      userDataValues.includes(password)
+    ) {
       return res.status(200).send({
         userName,
         accessToken: Jwt.signToken(userId),
